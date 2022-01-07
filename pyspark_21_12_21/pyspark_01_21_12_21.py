5b2dbb112e673459faced2208d6cfca443e670fb
bhanuprakashpothugnta <pothuguntabhanuprakash@gmail.com>
2022-01-07 18:24:11 +0530

    first commit

---
+++ pyspark_21_12_21/pyspark_01_21_12_21.py
@@ -0,0 +1,13 @@
+import pyspark
+from pyspark.sql import SparkSession
+if __name__ == "__main__":
+    print("Application started......")
+
+    spark = SparkSession.builder.master("local") \
+        .appName('SparkByExamples.com') \
+        .getOrCreate()
+    df = spark.read.csv('C:\\Users\\banup\\OneDrive\\Desktop\\spark\\emp1.txt', header=True)
+    df.show()
+
+    print("Application Completed......")
+
